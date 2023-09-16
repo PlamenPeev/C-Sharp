3c85623447f60d7f3eb8c211ad3bcf7618ef44f3
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-16 12:43:25 +0300

    Update Program.cs

--- 01.First Steps in Programming/Lab_FirstStepsInProgramming/01.HelloSoftUni/Lab_FirstStepsInProgramming/01.HelloSoftUni/02. TextReading/Program.cs
+++ 01.First Steps in Programming/Lab_FirstStepsInProgramming/01.HelloSoftUni/Lab_FirstStepsInProgramming/01.HelloSoftUni/02. TextReading/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = Console.ReadLine();
+                Console.WriteLine(text);
+        
         }
     }
 }
