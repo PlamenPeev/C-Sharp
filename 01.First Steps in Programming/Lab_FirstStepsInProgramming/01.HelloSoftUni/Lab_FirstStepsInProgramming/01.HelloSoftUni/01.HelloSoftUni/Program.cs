0d07ebce16efda49261a556a8db3b32e66277f23
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-16 12:40:09 +0300

    Add files via upload

---
+++ 01.First Steps in Programming/Lab_FirstStepsInProgramming/01.HelloSoftUni/Lab_FirstStepsInProgramming/01.HelloSoftUni/01.HelloSoftUni/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstStepsInProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, SoftUni!");
+        }
+    }
+}
