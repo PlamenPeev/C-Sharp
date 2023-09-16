1d6f07f3f4dbdae3caba9162724985004a7e3490
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-16 12:40:33 +0300

    Add files via upload

---
+++ 01.First Steps in Programming/Lab_FirstStepsInProgramming/01.HelloSoftUni/Lab_FirstStepsInProgramming/01.HelloSoftUni/03. Square Area/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _03._Square_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfSide = int.Parse(Console.ReadLine());
+            int area = lengthOfSide * lengthOfSide;
+            Console.WriteLine(area);
+        }
+    }
+}
