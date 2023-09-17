05b8444ec5b35de983bd3d9124aba1ff87265481
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-17 21:25:41 +0300

    Add files via upload

---
+++ 01.First Steps in Programming/Lab_FirstStepsInProgramming/01.HelloSoftUni/Lab_FirstStepsInProgramming/01.HelloSoftUni/04. InchesToCentimeters/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _04._InchesToCentimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            Console.WriteLine(centimeters);
+        }
+    }
+}
