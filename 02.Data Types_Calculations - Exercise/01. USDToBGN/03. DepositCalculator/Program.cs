aaac283fff4cb6585e4c50f07ee0b28059180bf7
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-20 18:32:59 +0300

    Add files via upload

---
+++ 02.Data Types_Calculations - Exercise/01. USDToBGN/03. DepositCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03._DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositedAmount = double.Parse(Console.ReadLine());
+            int termOfMonths = int.Parse(Console.ReadLine());
+            double annualRate = double.Parse(Console.ReadLine());
+
+            double amount = depositedAmount + termOfMonths * ((depositedAmount * annualRate / 100.0) / 12);
+
+            Console.WriteLine(amount);
+        }
+    }
+}
