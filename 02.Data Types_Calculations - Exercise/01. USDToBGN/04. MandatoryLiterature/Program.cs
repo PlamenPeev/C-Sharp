a070498c37355191830d83a5c7a8f2d59b567202
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-20 18:33:17 +0300

    Add files via upload

---
+++ 02.Data Types_Calculations - Exercise/01. USDToBGN/04. MandatoryLiterature/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04._MandatoryLiterature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOFPages = int.Parse(Console.ReadLine());
+            int pagesForOneHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int totalTimePerBook = numberOFPages / pagesForOneHour;
+
+            int hoursPerDay = totalTimePerBook / days;
+
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
