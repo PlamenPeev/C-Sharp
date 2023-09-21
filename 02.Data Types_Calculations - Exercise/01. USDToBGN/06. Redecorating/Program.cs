0fc4590dbd2b58892fe6e3b35f802257433e735f
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-21 21:18:37 +0300

    Add files via upload

---
+++ 02.Data Types_Calculations - Exercise/01. USDToBGN/06. Redecorating/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06._Redecorating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int amountOfNylon = int.Parse(Console.ReadLine());
+            int amountOfPaintInLiters = int.Parse(Console.ReadLine());
+            int quantityOfThinnerInLiters = int.Parse(Console.ReadLine());
+            int neededHoursForMaking = int.Parse(Console.ReadLine());
+
+            double priceOfNylon = (amountOfNylon + 2) * 1.50;
+            double priceOfPaint = (amountOfPaintInLiters * 0.1 + amountOfPaintInLiters) * 14.50;
+            double priceOfThinner = quantityOfThinnerInLiters * 5.00;
+
+            double totalPriceOfMaterials = priceOfNylon + priceOfPaint + priceOfThinner + 0.40;
+            double amountForCraftsmen = (totalPriceOfMaterials * 0.3) * neededHoursForMaking;
+            double total = totalPriceOfMaterials + amountForCraftsmen;
+
+            Console.WriteLine(total);
+        }
+    }
+}
