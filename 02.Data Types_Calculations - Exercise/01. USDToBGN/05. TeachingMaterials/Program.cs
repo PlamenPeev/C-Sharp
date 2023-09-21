6a9efde207063b4eda62e8cf1ba5779b87a41701
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-21 21:18:24 +0300

    Add files via upload

---
+++ 02.Data Types_Calculations - Exercise/01. USDToBGN/05. TeachingMaterials/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05._TeachingMaterials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int packageOfPens = int.Parse(Console.ReadLine());
+            int packageOfMarkers = int.Parse(Console.ReadLine());
+            int boardCleanerPerLiter = int.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+
+            double priceOfPens = packageOfPens * 5.80;
+            double priceOfMarkers = packageOfMarkers * 7.20;
+            double priceOfCleaner = boardCleanerPerLiter * 1.20;
+
+            double priceOfAll = priceOfPens + priceOfMarkers + priceOfCleaner;
+            double totalAmount = priceOfAll - (priceOfAll * discount / 100.0);
+
+            Console.WriteLine(totalAmount);
+        }
+    }
+}
