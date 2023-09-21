31e271469ab43bf64ddeae748f869a86dc804227
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-21 21:18:53 +0300

    Add files via upload

---
+++ 02.Data Types_Calculations - Exercise/01. USDToBGN/07. FoodDelivery/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _07._FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int veganMenu = int.Parse(Console.ReadLine());
+
+            double priceOfChicken = chickenMenu * 10.35;
+            double priceOfFish = fishMenu * 12.40;
+            double priceOfVegan = veganMenu * 8.15;
+
+            double costOfAllMenues = priceOfChicken + priceOfFish + priceOfVegan;
+            double priceOfDessert = costOfAllMenues * 0.2;
+
+            double orderPrice = costOfAllMenues + priceOfDessert + 2.50;
+            Console.WriteLine(orderPrice);
+        }
+    }
+}
