c92d131d49ae0af2f28b93db1c03f6174ba6934f
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-24 12:42:19 +0300

    Add files via upload

---
+++ 02.Data Types_Calculations - Exercise/01. USDToBGN/09. Aquarium/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _09._Aquarium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            int volumeOfAquarium = length * width * height;
+            double volumeInLiters = volumeOfAquarium * 0.001;
+
+            double neededLiters = volumeInLiters * (1 - percentage / 100.0);
+            Console.WriteLine($"{neededLiters:f2}");
+        }
+    }
+}
