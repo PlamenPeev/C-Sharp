8bc057dfb46cef01a02bdb74282af1eff3e0e113
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-24 12:42:07 +0300

    Add files via upload

---
+++ 02.Data Types_Calculations - Exercise/01. USDToBGN/08. BasketballEquipment/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _08._BasketballEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int annualFee = int.Parse(Console.ReadLine());
+
+            double sneakers = annualFee * 0.6;
+            double team = sneakers * 0.8;
+            double basketball = team * 0.25;
+            double accessories = basketball * 0.2;
+
+            double priceOfEquipment = annualFee + sneakers + team + basketball + accessories;
+            Console.WriteLine(priceOfEquipment);
+        }
+    }
+}
