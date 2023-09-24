5be22b30f0753a45ec08d1a81ae4e11f34f5548a
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-09-24 12:45:53 +0300

    Add files via upload

---
+++ 03.Conditional Statements_Nested Conditional Statements - Exercise/01. Cinema/01. Cinema/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Cinema;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string typeOfScreening = Console.ReadLine();
+        int rows = int.Parse(Console.ReadLine());
+        int columns = int.Parse(Console.ReadLine());
+
+        double income = 0.0;
+        int totalChairs = rows * columns;
+
+        if (typeOfScreening == "Premiere")
+        {
+            income = totalChairs * 12.00;
+        }
+        else if (typeOfScreening == "Normal")
+        {
+            income = totalChairs * 7.50;
+        }
+        if (typeOfScreening == "Discount")
+        {
+            income = totalChairs * 5.00;
+        }
+
+        Console.WriteLine($"{income:F2} leva");
+    }
+}
