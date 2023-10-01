8f72259e962854282a0a857329ee92deff789d95
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-01 14:16:59 +0300

    Add files via upload

---
+++ 3.2.Conditional Statement - more exercise/1. FreezingWeather/1. FreezingWeather/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _1._FreezingWeather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperature = double.Parse(Console.ReadLine());
+
+            if(temperature <= 0)
+            {
+                Console.WriteLine("Freezing weather!");
+            }
+        }
+    }
+}
