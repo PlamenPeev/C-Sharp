a02a5eb8f27d6870c747ea5d5492f8b98136a60b
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-01 14:13:41 +0300

    Add files via upload

---
+++ 3.1.Complex-Conditional-Statements-Lab/01. Market Place/09. Cinema/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _09._Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int seats = int.Parse(Console.ReadLine());
+
+            int allSeats = rows * seats;
+            double price = 0.0;
+
+            if(type == "Premiere")
+            {
+                price = allSeats * 12.00;
+            }
+            else if (type == "Normal")
+            {
+                price = allSeats * 7.50;
+            }
+            else if (type == "Discount")
+            {
+                price = allSeats * 5.00;
+            }
+
+            Console.WriteLine("{0:f2}",price);
+        }
+    }
+}
