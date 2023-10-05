8f0c71e8df200cc82ce06dfb111f6e6d4833ac7c
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-05 19:46:26 +0300

    Add files via upload

---
+++ 3.2.Conditional Statement - more exercise/1. FreezingWeather/10. ValidTriangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+
+namespace _10._ValidTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            bool isValidTriangle = true;
+
+            if (a + b <= c)
+            {
+                isValidTriangle = false;
+            }
+            else if (a + c <= b)
+            {
+                isValidTriangle = false;
+            }
+            else if (b + c <= a)
+            {
+                isValidTriangle = false;
+            }
+            else
+            {
+                isValidTriangle = true;
+            }
+
+            if (isValidTriangle)
+            {
+                Console.WriteLine("Valid Triangle");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Triangle");
+
+            }
+        }
+    }
+}
