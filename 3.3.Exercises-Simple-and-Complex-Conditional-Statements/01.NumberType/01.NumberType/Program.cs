4b5d3887fb1c814472d07016a31a1603912caced
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-06 21:02:43 +0300

    Add files via upload

---
+++ 3.3.Exercises-Simple-and-Complex-Conditional-Statements/01.NumberType/01.NumberType/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.NumberType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if(num == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("positive");
+            }
+
+        }        
+    }
+}
