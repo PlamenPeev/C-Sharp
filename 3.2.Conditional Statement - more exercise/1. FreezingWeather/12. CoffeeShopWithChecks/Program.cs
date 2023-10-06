172e165c4f83f3cd72ad8d897fed2e8f4bce2842
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-06 20:57:15 +0300

    Add files via upload

---
+++ 3.2.Conditional Statement - more exercise/1. FreezingWeather/12. CoffeeShopWithChecks/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _12._CoffeeShopWithChecks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string typeOfDrink = Console.ReadLine();
+            string extra = Console.ReadLine();
+
+            double coffeePrice = 1.00;
+            double teaPrice = 0.60;
+            double sugarPrice = 0.40;
+
+            double price = 0.0;
+
+            bool isValid = false;
+
+
+            if (typeOfDrink == "coffee")
+            {
+                if (extra == "sugar")
+                {
+                    price = coffeePrice + sugarPrice;
+                    Console.WriteLine("Final price: ${0:f2}", price);
+                }
+                else if (extra == "no")
+                {
+                    price = coffeePrice;
+                    Console.WriteLine("Final price: ${0:f2}", price);
+                }
+                else
+                {
+                    Console.WriteLine("Unknown extra");
+                }
+
+               
+            }
+            else if (typeOfDrink == "tea")
+            {
+                if (extra == "sugar")
+                {
+                    price = teaPrice + sugarPrice;
+                    Console.WriteLine("Final price: ${0:f2}", price);
+                }
+                else if (extra == "no")
+                {
+                    price = teaPrice;
+                    Console.WriteLine("Final price: ${0:f2}", price);
+                }
+                else
+                {
+                    Console.WriteLine("Unknown extra");
+                }
+
+                
+            }
+            else
+            {
+                Console.WriteLine("Unknown drink");
+            }
+
+
+            
+        }
+    }
+}
