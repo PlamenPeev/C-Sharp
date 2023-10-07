d3a04b82f7443118250b0bc555ff84e887d72eb6
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-07 21:49:30 +0300

    Add files via upload

---
+++ 3.3.Exercises-Simple-and-Complex-Conditional-Statements/01.NumberType/04. NumberOperations/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04._NumberOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            string operatorStr = Console.ReadLine();
+
+            double result = 0;
+
+            if(operatorStr == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (operatorStr == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (operatorStr == "*")
+            {
+                result = num1 * num2;
+            }
+            else if (operatorStr == "/")
+            {
+                result = num1 / num2;
+            }
+
+            Console.WriteLine($"{num1} {operatorStr} {num2} = {result:f2}");
+
+        }
+    }
+}
