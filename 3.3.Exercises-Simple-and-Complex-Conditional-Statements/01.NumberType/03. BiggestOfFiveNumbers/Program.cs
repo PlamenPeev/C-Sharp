4ffc141daf4ed4e827448462220b1626c7a2703d
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-07 21:49:17 +0300

    Add files via upload

---
+++ 3.3.Exercises-Simple-and-Complex-Conditional-Statements/01.NumberType/03. BiggestOfFiveNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03._BiggestOfFiveNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNum = int.MinValue;
+            
+            for(int i = 1; i <= 5; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+            }
+
+            Console.WriteLine(maxNum);
+        }
+    }
+}
