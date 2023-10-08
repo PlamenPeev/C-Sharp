5b74a7a9d19f4d547d101cbdf385876ad6cd7277
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-08 18:12:10 +0300

    Add files via upload

---
+++ 3.3.Exercises-Simple-and-Complex-Conditional-Statements/01.NumberType/06. Product OfThreeNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _06._Product_OfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int countNegativNum = 0;
+
+            if(num1 < 0)
+            {
+                countNegativNum++;
+            }
+            if (num2 < 0)
+            {
+                countNegativNum++;
+            }
+            if (num3 < 0)
+            {
+                countNegativNum++;
+            }
+
+
+
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if(countNegativNum == 0 || countNegativNum == 2)
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
