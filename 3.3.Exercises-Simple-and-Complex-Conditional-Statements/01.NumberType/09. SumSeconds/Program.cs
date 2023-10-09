ad81a7d864f8651b24d723a9fac6fa38d9c1affe
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-09 18:55:28 +0300

    Add files via upload

---
+++ 3.3.Exercises-Simple-and-Complex-Conditional-Statements/01.NumberType/09. SumSeconds/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _09._SumSeconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sumTime = num1 + num2 + num3;
+            int minutes = sumTime / 60;
+            int seconds = sumTime % 60;
+
+            string zeroSymbol = "";
+            if(seconds < 10)
+            {
+                zeroSymbol = "0";
+            }
+
+            Console.WriteLine($"{minutes}:{zeroSymbol}{seconds}");
+        }
+    }
+}
