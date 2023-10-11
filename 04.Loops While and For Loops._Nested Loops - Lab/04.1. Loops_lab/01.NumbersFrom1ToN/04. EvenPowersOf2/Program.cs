0b988b5a599b846e9243a2fcb88169cad6f2233e
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-11 22:22:10 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.1. Loops_lab/01.NumbersFrom1ToN/04. EvenPowersOf2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04._EvenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+
+            for (int i = 1; i <= n + 2; i ++)
+            {
+                if( i % 2 == 0)
+                {
+                   Console.WriteLine(num);
+                    num = (int)Math.Pow(2, i);
+                }
+                
+            }
+        }
+    }
+}
