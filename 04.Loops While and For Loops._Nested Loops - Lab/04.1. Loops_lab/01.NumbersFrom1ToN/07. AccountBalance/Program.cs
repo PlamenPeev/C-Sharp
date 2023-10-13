1b3f2e1f2864159171fb02913f7e2daff8e0df39
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-13 21:11:27 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.1. Loops_lab/01.NumbersFrom1ToN/07. AccountBalance/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _7._1._AccountBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double sum = 0;
+
+            while (command != "NoMoreMoney")
+            {
+
+                double currentNum = double.Parse(command);
+
+                if (currentNum > 0)
+                {
+                    Console.WriteLine($"Increase: {currentNum:F2}");
+                    sum += currentNum;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                   
+                
+                command = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
