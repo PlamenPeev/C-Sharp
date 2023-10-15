2972a6c9d71188f6ac946d704891236e0f676701
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-15 22:37:57 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.1. Loops_lab/01.NumbersFrom1ToN/10. Graduation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace _10._Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            int year = 0;
+            double totalSumOfGrades = 0;
+            int failed = 0;
+
+            while (year < 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if(grade < 4)
+                {
+                    failed++;
+                }
+
+                if(failed > 1)
+                {
+                    break;
+                }
+
+                year++;
+                totalSumOfGrades += grade;
+            }
+
+            double averageGrade = totalSumOfGrades / 12.0;
+
+            if (failed > 1)
+            {
+                Console.WriteLine($"{studentName} has been excluded at {year} grade");
+            }
+            else
+            {
+                Console.WriteLine($"{studentName} graduated. Average grade: {averageGrade:F2}");
+            }
+        }
+    }
+}
