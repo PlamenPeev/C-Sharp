554c94f8e9568c91af7c9fd41148d249d1670113
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-16 19:31:37 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.2.Simple_Loops_exercise/01. NumbersEndingIn7/02. HalfSumElement/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02._HalfSumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int maxNum = int.MinValue;
+
+            for ( int i = 1; i <= n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+
+                if (currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+
+            }
+
+          
+             int finalSum = sum - maxNum;
+
+            if (maxNum == finalSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(maxNum - (sum - maxNum))}");
+            }
+        }
+    }
+}
