87f2a4dc91e54c2a52432a8cae14989cc49a4ee9
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-16 19:32:23 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.2.Simple_Loops_exercise/01. NumbersEndingIn7/04. ExamPreparation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.Tracing;
+using System.Xml.Linq;
+
+namespace _04._ExamPreparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPoorGrades = int.Parse(Console.ReadLine());
+
+            int counterPoorGrades = 0;
+            double sumGrade = 0;
+            int countGrades = 0;
+            int numberOfProblems = 0;
+            string currentProblem = "";
+
+            while(true)
+            {
+                string problemName = Console.ReadLine();
+
+                if (problemName == "Enough")
+                {
+                    break;
+                }
+
+                int grade = int.Parse(Console.ReadLine());
+
+                sumGrade += grade;
+                countGrades++;
+                numberOfProblems++;
+                currentProblem = problemName;
+
+                if(grade <= 4)
+                {
+                    counterPoorGrades++;
+                }
+
+                if(counterPoorGrades == numOfPoorGrades)
+                {
+                    break;
+                }
+
+            }
+
+            double averageGrade = sumGrade / countGrades;
+
+            if(counterPoorGrades == numOfPoorGrades)
+            {
+                Console.WriteLine($"You need a break, {numOfPoorGrades} poor grades.");
+            }
+            else
+            {
+                Console.WriteLine($"Average score: {averageGrade:f2}");
+                Console.WriteLine($"Number of problems: {numberOfProblems}");
+                Console.WriteLine($"Last problem: {currentProblem}");
+
+            }
+        }
+    }
+}
