6b7dae5e76c97f3d96074708259ff2ffe12ee05f
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-16 19:32:01 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.2.Simple_Loops_exercise/01. NumbersEndingIn7/03. OldBooks/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03._OldBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string titleOfTheBook = Console.ReadLine();
+
+            int count = 0;
+            bool isBookFound = false;
+
+            string nextBookName = Console.ReadLine();
+
+            while(nextBookName != "No More Books")
+            {
+                if(nextBookName == titleOfTheBook)
+                {
+                    isBookFound = true;
+                    break;
+                }
+
+                count++;
+                nextBookName = Console.ReadLine();
+            }
+
+            if (isBookFound)
+            {
+                Console.WriteLine($"You checked {count} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {count} books.");
+            }
+        }
+    }
+}
