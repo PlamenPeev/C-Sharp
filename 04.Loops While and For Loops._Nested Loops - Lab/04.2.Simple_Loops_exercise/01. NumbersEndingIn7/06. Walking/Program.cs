1cfd59f430ea96b88c1ef08606b3e1f492a3f4d0
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-17 18:37:59 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.2.Simple_Loops_exercise/01. NumbersEndingIn7/06. Walking/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _06._Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int goalReached = 10000;
+            bool isGoalReached = false;
+            bool isGoingHome = false;
+            int sumOfSteps = 0;
+
+            while(true)
+            {
+                
+                if( input == "Going home")
+                {
+                    isGoingHome = true;
+                    input = Console.ReadLine();
+                    sumOfSteps += int.Parse(input);
+                    break;
+                }
+
+                int steps = int.Parse(input);
+                sumOfSteps += steps;
+
+                if(sumOfSteps > goalReached)
+                {
+                    isGoalReached = true;
+                    break;
+                }
+                input = Console.ReadLine();
+
+            }
+
+            if (isGoingHome)
+            {
+                if(sumOfSteps > goalReached)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{sumOfSteps - goalReached} steps over the goal!");
+                }
+                else
+                {
+                    Console.WriteLine($"{goalReached - sumOfSteps} more steps to reach goal.");
+                }
+            }
+            else if( isGoalReached)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{sumOfSteps - goalReached} steps over the goal!");
+            }
+
+
+
+
+        }
+    }
+}
