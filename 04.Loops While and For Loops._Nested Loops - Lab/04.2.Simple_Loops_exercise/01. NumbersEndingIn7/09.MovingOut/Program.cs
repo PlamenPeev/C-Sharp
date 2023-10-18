5cc6b4bf23eca5c6c92b4f9605d0479964287b46
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-18 20:21:04 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.2.Simple_Loops_exercise/01. NumbersEndingIn7/09.MovingOut/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _9._1.MovingOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int availablePlace = width * length * height;
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                int element = 
+            }
+
+        }
+    }
+}
