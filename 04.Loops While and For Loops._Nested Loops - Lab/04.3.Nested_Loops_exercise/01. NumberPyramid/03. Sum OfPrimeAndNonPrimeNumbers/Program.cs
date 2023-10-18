25576394fd194481cd37863cd0bccf393a8f34b0
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-18 20:23:13 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.3.Nested_Loops_exercise/01. NumberPyramid/03. Sum OfPrimeAndNonPrimeNumbers/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _03._Sum_OfPrimeAndNonPrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int primeSum = 0;
+            int nonPrimeSum = 0;
+
+
+            string input = Console.ReadLine();
+            while ( input != "stop")
+            {
+                int number = int.Parse(input);
+
+                if(number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                //bool isPrime = true;
+                //for (int i = 2; i < number; i++)
+                //{
+                //    if (number % i == 0)
+                //    {
+                //        isPrime = false;
+                //        break;
+                //    }
+                //}
+
+                //if (isPrime == true)
+                //{
+                //    primeSum += number;
+                //}
+                //else
+                //{
+                //    nonPrimeSum += number;
+                //}
+
+
+                int divisors = 0;
+                for (int i = 1; i <= number; i++)
+                    if (number % i == 0)
+                        divisors++;
+
+                if (divisors == 2)
+                {
+                    primeSum += number;
+                }
+                else
+                {
+                    nonPrimeSum += number;
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+    }
+}
