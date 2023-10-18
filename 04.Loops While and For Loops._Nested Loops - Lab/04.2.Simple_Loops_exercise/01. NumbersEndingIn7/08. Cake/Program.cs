26da71a2b20d9cc9aacc3f1a397270fe2d76b953
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-18 20:20:49 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.2.Simple_Loops_exercise/01. NumbersEndingIn7/08. Cake/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _08._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            int numberOfPieces = width * length;
+            string input = Console.ReadLine();
+            bool isNoMoreCake = false;
+           
+
+            while (true)
+            {
+
+                if( input == "STOP")
+                {
+                    break;
+                }
+
+                int takePieces = int.Parse(input);
+                numberOfPieces -= takePieces;
+
+                if(numberOfPieces < 0)
+                {
+                    isNoMoreCake = true;
+                    break;
+                }
+
+                    input = Console.ReadLine();
+            }
+
+            if (isNoMoreCake)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(numberOfPieces)} pieces more.");
+            }
+            else
+            {
+                Console.WriteLine($"{numberOfPieces} pieces are left.");
+            }
+        }
+    }
+}
