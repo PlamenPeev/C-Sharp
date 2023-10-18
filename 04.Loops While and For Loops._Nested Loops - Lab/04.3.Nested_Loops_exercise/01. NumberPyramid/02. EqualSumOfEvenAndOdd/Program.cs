38a228ed279d99bba288589dc39cf03fd83df93d
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-18 20:22:48 +0300

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.3.Nested_Loops_exercise/01. NumberPyramid/02. EqualSumOfEvenAndOdd/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _02._EqualSumOfEvenAndOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                string currentNumber = i.ToString();
+                int evenSum = 0;
+                int oddSun = 0;
+
+                for (int j = 0; j < currentNumber.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentNumber[j].ToString());
+
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSun += currentDigit;
+                    }
+                }
+
+                if (evenSum == oddSun)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
