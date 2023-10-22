c0bfc63b21e94299f512236f5178cb1da4cc87b6
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-22 11:44:41 +0300

    Add files via upload

---
+++ 5.1.Nested-Loops-Lab/01.NumbersFromNto1/06.TravelSavings/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.TravelSavings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            while(input != "End") {
+
+                string destination = input;
+                double budjet = double.Parse(Console.ReadLine());
+                double sum = 0;
+
+                while (sum < budjet)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    sum += money;
+                    Console.WriteLine($"Collected: {sum:f2}");
+                }
+
+                if (sum >= budjet)
+                {
+                    Console.WriteLine($"Going to {destination}!");
+                }
+                sum = 0;
+                
+                input = Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
