286df6c1a5337685625e565b38dc6d7d6c4fb29f
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-22 11:44:14 +0300

    Add files via upload

---
+++ 5.1.Nested-Loops-Lab/01.NumbersFromNto1/04.Building/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            for (int i = floors; i >= 1; i--)
+            {
+                string printLine = "";
+                for (int j = 0; j < rooms; j++)
+                {
+
+                    if (i == floors)
+                    {
+                        printLine += $"L{ i}{ j} ";
+                    }
+                    else
+                    {
+                        if (i % 2 == 0)
+                        {
+                            printLine += $"O{ i}{ j} ";
+                        }
+                        else
+                        {
+                            printLine += $"A{ i}{ j} ";
+                        }
+                    }
+                }
+                Console.WriteLine(printLine);
+            }
+        }
+    }
+}
