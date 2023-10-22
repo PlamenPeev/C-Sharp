5a3b7f193f3f8ddcb734df33208e55a0cc1efd5a
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-22 11:44:28 +0300

    Add files via upload

---
+++ 5.1.Nested-Loops-Lab/01.NumbersFromNto1/05.NumberPyramid/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05.NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int current = 1;
+            bool isBigger = false;
+            string printCurrentLine = "";
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    if (current > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    printCurrentLine += current + " ";
+                    current++;
+                }
+                Console.WriteLine(printCurrentLine);
+                printCurrentLine = "";
+                if (isBigger)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
