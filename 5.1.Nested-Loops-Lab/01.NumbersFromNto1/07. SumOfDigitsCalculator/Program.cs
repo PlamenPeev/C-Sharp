bead0bf7e62a72b00c8802cca7fc0c6edc3f3688
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-23 23:12:26 +0300

    Add files via upload

---
+++ 5.1.Nested-Loops-Lab/01.NumbersFromNto1/07. SumOfDigitsCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07._SumOfDigitsCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine(); 
+
+
+            while( number != "End") {
+                int sum = 0;
+
+                for (int i = 0; i <= number.Length - 1; i++)
+                {
+                    string currentDigit = number.Substring(i, 1);
+                    int currentNum = int.Parse(currentDigit);
+                    sum += currentNum;
+                }
+                Console.WriteLine($"Sum of digits = {sum}");
+                sum = 0;
+
+                number = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
