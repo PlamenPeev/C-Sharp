c020071e7e16899411c060a22c8a6de04b6f0a68
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-23 23:18:14 +0300

    Add files via upload

---
+++ 5.2.Methods-Lab/01. SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01._SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            PrintSign(number);
+        }
+
+        private static void PrintSign(int number) { 
+        
+        if(number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+        else if(number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        
+        }
+    }
+}
