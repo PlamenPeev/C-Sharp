6b5ac2cebf16ac74dbe6cc8b70ad14c94276509f
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-23 23:14:49 +0300

    Add files via upload

---
+++ 5.1.Nested-Loops-Lab/01.NumbersFromNto1/8.4.PrimeNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _8._4.PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+
+            for (int num = start; num <= end; num++)
+            {
+                if (IsPrime(num))
+                {
+                    Console.Write(num + " ");
+                }
+            }
+
+
+        }
+        static bool IsPrime(int number)
+        {
+            if (number < 1)
+            {
+                return false;
+            }
+
+
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
