501b32915b061a042553694f2eb916cf6b5bc0fb
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-24 22:08:08 +0300

    Add files via upload

---
+++ 5.2.Methods-Lab/01. SignOfIntegerNumbers/04. CalculateRectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04._CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            printRectangleArea(width, length);
+        }
+
+        private static void printRectangleArea( int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+    }
+}
