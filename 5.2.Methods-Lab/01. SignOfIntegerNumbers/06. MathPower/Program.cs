5bd0e3b636ef48ca5e12d6be7c293a605ec2019f
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-25 23:30:21 +0300

    Add files via upload

---
+++ 5.2.Methods-Lab/01. SignOfIntegerNumbers/06. MathPower/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06._MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            NumOfPower(num, power);
+        }
+
+
+       private static void NumOfPower(int n, int p)
+        {
+            Console.WriteLine(Math.Pow(n,p));
+        }
+    }
+}
