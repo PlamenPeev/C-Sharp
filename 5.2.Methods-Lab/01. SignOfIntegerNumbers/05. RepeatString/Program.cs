181bc87af5992d6ceb75588451243b3b99fc277f
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-25 23:30:08 +0300

    Add files via upload

---
+++ 5.2.Methods-Lab/01. SignOfIntegerNumbers/05. RepeatString/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace _05._RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int repeatNum = int.Parse(Console.ReadLine());
+            RepeatString(text, repeatNum);
+        }
+
+        static void RepeatString(string text, int num)
+        {
+            for(int i = 1; i <= num; i++)
+            {
+                Console.Write(text);
+            }
+        }
+    }
+}
