c2afece5c373253fc60e50cef9ba5481a08332ba
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-26 21:48:03 +0300

    Add files via upload

---
+++ 5.2.Methods-Lab/01. SignOfIntegerNumbers/08.1. MultiplyEvenByOdd/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _08._1._MultiplyEvenByOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int result = getMultiplyEvensByOdds(number);
+            Console.WriteLine(result);
+        }
+
+        private static int getMultiplyEvensByOdds(int number)
+        {
+            int evenSum = getEvenSum(number);
+            int oddSum = getOddSum(number);
+            return evenSum * oddSum;
+        }
+
+        private static int getOddSum(int number)
+        {
+            int sum = 0;
+            while (Math.Abs(number) > 0)
+            {
+                int lastDigit = number % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+
+        private static int getEvenSum(int number)
+        {
+            int sum = 0;
+            while (Math.Abs(number) > 0)
+            {
+                int lastDigit = number % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    sum += lastDigit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
