ddf3b74ce02150120480fbe5e9a176ad17a99146
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-26 21:48:18 +0300

    Add files via upload

---
+++ 5.2.Methods-Lab/01. SignOfIntegerNumbers/09. Orders/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _09._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            totalPriceInOrder(product, quantity);
+        }
+
+        static void totalPriceInOrder(string product, int quantity)
+        {
+            double price = 0.0;
+
+            switch(product)
+            {
+                case "coffee":
+                    price = quantity * 1.50;
+                    break;
+                case "water":
+                    price = quantity * 1.00;
+                    break;
+                case "coke":
+                    price = quantity * 1.40;
+                    break;
+                case "snacks":
+                    price = quantity * 2.00;
+                    break;
+                default:
+                    break;
+
+            }
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
