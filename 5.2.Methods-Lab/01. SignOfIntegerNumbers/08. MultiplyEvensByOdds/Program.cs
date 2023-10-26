8f56b284df2fa171a85357025f57175a19dfbdea
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-26 21:47:48 +0300

    Add files via upload

---
+++ 5.2.Methods-Lab/01. SignOfIntegerNumbers/08. MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _08._MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            MultiplyEvensByOdds(number);
+        }
+
+        static void MultiplyEvensByOdds(int number)
+        {
+            int sumEven = 0;
+            int sumOdd = 0;
+          
+            while(Math.Abs(number) > 0)
+            {
+                int currentNum = number % 10;
+                if(currentNum % 2 == 0)
+                {
+                    sumEven += currentNum;
+                }
+                else
+                {
+                    sumOdd += currentNum;
+                }
+                number /= 10;
+            }
+
+            Console.WriteLine(sumEven * sumOdd);
+
+        }
+    }
+}
