497ff32d9554d4a2e703908e7a1baf19303ea29b
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-27 20:58:38 +0300

    Add files via upload

---
+++ 5.3.Nested-Loops-And-Methods-Exercise/01.StupidPasswords/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.StupidPasswords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            validatePassword(number);
+        }
+
+        private static void validatePassword(int number)
+        {
+            for (int i = 2; i <= number; i++)
+            {
+                for (int j = 1; j <= number; j++)
+                {
+
+                    if (i % 2 == 0 && j % 2 != 0)
+                    {
+                        Console.Write($"{i}{j}{i * j} ");
+                    }
+                }
+            }
+        }
+    }
+}
