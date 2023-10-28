13d84fd70c89931ee1fb49bc541b4ea7428c0524
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-28 21:50:27 +0300

    Add files via upload

---
+++ 5.3.Nested-Loops-And-Methods-Exercise/01.StupidPasswords/04. LetterCombinations/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04._LetterCombinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char s = char.Parse(Console.ReadLine());
+            char e = char.Parse(Console.ReadLine());
+            char x = char.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for(char i = s; i <= e; i++)
+            {
+                for (char j = s; j <= e; j++)
+                {
+                    for (char k = s; k <= e; k++)
+                    {
+                        if( !i.Equals(x) && !j.Equals(x) && !k.Equals(x))
+                        {
+                            Console.Write($"{i}{j}{k} ");
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(counter);
+        }
+    }
+}
