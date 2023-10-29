9372272c6c446a87a8de22eecf50c0e6cda76ac0
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-29 14:36:28 +0200

    Add files via upload

---
+++ 5.3.Nested-Loops-And-Methods-Exercise/01.StupidPasswords/07. FactorialDivision/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07._FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+
+            double f1 = getFactorial(num1);
+            double f2 = getFactorial(num2);
+
+            double divide = f1 / f2;
+            Console.WriteLine(divide);
+        }
+
+        private static double getFactorial(double num){
+            double factorial = 1;
+            for(double i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
