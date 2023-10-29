cfcf77121060e6db1f1bebb68bddfc5a94314699
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-29 14:36:14 +0200

    Add files via upload

---
+++ 5.3.Nested-Loops-And-Methods-Exercise/01.StupidPasswords/06. VowelsCount/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _06._VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            countVowels(text);
+        }
+
+        private static void countVowels(string text)
+        {
+            int counter = 0;
+
+            for (int i = 0; i <= text.Length - 1; i++)
+            {
+                if (text[i] == 'a' || text[i] == 'A')
+                {
+                    counter++;
+                }
+                if (text[i] == 'e' || text[i] == 'E')
+                {
+                    counter++;
+                }
+                if (text[i] == 'i' || text[i] == 'I')
+                {
+                    counter++;
+                }
+                if (text[i] == 'o' || text[i] == 'O')
+                {
+                    counter++;
+                }
+                if (text[i] == 'u' || text[i] == 'U')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
