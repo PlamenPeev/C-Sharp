890c5193b51dd9c51b88c80c4b8b6887f80403a1
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-29 14:36:03 +0200

    Add files via upload

---
+++ 5.3.Nested-Loops-And-Methods-Exercise/01.StupidPasswords/05.HappyNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _05.HappyNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 0; j <= number; j++)
+                {
+                    for (int k = 0; k <= number; k++)
+                    {
+                        for(int l = 0; l <= number; l++)
+                        {
+                              if (i + j == number && k + l == number && i + j == k + l)
+                        
+                               Console.Write($"{i}{j}{k}{l} ");
+                        }
+                     
+                    }
+                }
+            }
+        }
+    }
+}
