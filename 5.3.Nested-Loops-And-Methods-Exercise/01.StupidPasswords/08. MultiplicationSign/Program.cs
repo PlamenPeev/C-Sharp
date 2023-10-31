1a146a0d3133c14ee311a37cc673cb210207099e
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-10-31 18:28:15 +0200

    Add files via upload

---
+++ 5.3.Nested-Loops-And-Methods-Exercise/01.StupidPasswords/08. MultiplicationSign/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _08._MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            multiplicationSign(n1, n2, n3);
+        }
+
+        private static void multiplicationSign(int n1, int n2, int n3)
+        {
+            int result = n1 * n2 * n3;
+
+            if (result > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (result < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("zero");
+            }
+        }
+    }
+}
