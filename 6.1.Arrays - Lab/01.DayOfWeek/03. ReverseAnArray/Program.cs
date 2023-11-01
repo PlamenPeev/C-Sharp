ae49c5d98dd15ba9d64a602fc942ad38589bef91
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-01 18:27:46 +0200

    Add files via upload

---
+++ 6.1.Arrays - Lab/01.DayOfWeek/03. ReverseAnArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03._ReverseAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n]; 
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Console.WriteLine(string.Join(", ",arr));
+
+            for(int i = n-1; i >= 0; i--)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}
