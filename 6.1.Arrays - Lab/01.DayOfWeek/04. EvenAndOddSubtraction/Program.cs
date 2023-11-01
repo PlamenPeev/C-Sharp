cbb5b77751cf4d316c0ddcc9ff8e8681fd0a9ed3
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-01 18:28:01 +0200

    Add files via upload

---
+++ 6.1.Arrays - Lab/01.DayOfWeek/04. EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04._EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ")
+                .Select(int.Parse).ToArray();
+
+            int sumEvens = 0;
+            int sumOdds = 0;
+
+            for(int i  = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumEvens += numbers[i];
+                }
+                else
+                {
+                    sumOdds += numbers[i];
+                }
+            }
+
+            int difference = sumEvens - sumOdds;
+            Console.WriteLine(difference);
+        }
+    }
+}
