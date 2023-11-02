1cea39500cfd2fb288789c68d41dba2597588cb9
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-02 19:00:52 +0200

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.3.Nested_Loops_exercise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryMembers = int.Parse(Console.ReadLine());
+
+            double totalScore = 0;
+            int numOfPresantation = 0;
+            while (true)
+            {
+                string nameOfPresantation = Console.ReadLine();
+
+                if( nameOfPresantation == "Finish")
+                {
+                    break;
+                }
+                double sumOfGrades = 0;
+                numOfPresantation++;
+                
+                for(int i = 1; i <= juryMembers; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    sumOfGrades += grade;
+                    totalScore += grade;
+                }
+
+                Console.WriteLine($"{nameOfPresantation} - {(sumOfGrades / juryMembers):f2}.");
+            }
+
+            Console.WriteLine($"Student's final assessment is {(totalScore / (numOfPresantation * juryMembers)):f2}.");
+        }
+    }
+}
