9f54ac931bfc979edca5d1c4f489a94ed84dc8dc
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-02 18:58:02 +0200

    Add files via upload

---
+++ 6.2.List-Lab/01.ChangeList/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            string input = Console.ReadLine();
+
+            while(input != "end")
+            {
+                string command = input.Split(" ")[0];
+                int element = int.Parse(input.Split(" ")[1]);
+
+                if(command == "Delete")
+                {
+                    numbers.RemoveAll(x => x == element);
+                }
+                else if(command == "Insert")
+                {
+                    int position = int.Parse(input.Split(" ")[2]);
+                    numbers.Insert(position, element);
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
