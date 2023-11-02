3de22b7a34f072a80820bc6d554456bc51baa62b
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-02 19:01:31 +0200

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.3.Nested_Loops_exercise/06. CinemaTickets/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _06._CinemaTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int studentTicket = 0;
+            int standardTicket = 0;
+            int kidTicket = 0;
+
+            while (true)
+            {
+                string nameOfMovie = Console.ReadLine();
+                if(nameOfMovie == "Finish")
+                {
+                    break;
+                }
+                int availableSeats = int.Parse(Console.ReadLine());
+
+                int soldTickets = 0;
+                while (soldTickets < availableSeats)
+                {
+                    string typeTicket = Console.ReadLine();
+                    if (typeTicket == "End")
+                    {
+                        break;
+                    }
+
+                    soldTickets++;
+
+                    if (typeTicket == "student")
+                    {
+                        studentTicket++; 
+                    }
+                    else if (typeTicket == "standard")
+                    {
+                        standardTicket++;
+                    }
+                    else if (typeTicket == "kid")
+                    {
+                        kidTicket++; 
+                    }
+                }
+                Console.WriteLine($"{nameOfMovie} - {100.0 * soldTickets / availableSeats:f2}% full.");
+
+            }
+            int totalTickets = studentTicket + standardTicket + kidTicket;
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            Console.WriteLine($"{100.0 * studentTicket / totalTickets:f2}% student tickets.");
+            Console.WriteLine($"{100.0 * standardTicket / totalTickets:f2}% standard tickets.");
+            Console.WriteLine($"{100.0 * kidTicket / totalTickets:f2}% kids tickets.");
+        }
+
+    }
+}
