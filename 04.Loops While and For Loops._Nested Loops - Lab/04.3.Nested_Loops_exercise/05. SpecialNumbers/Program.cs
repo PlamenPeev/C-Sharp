d2473d9b49708e390d0bc2a154aa85aaef5055be
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-02 19:01:11 +0200

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.3.Nested_Loops_exercise/05. SpecialNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05._SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int specialNum = 0;
+            for(int i = 1111; i <= 9999; i++)
+            {
+                int count = 0;
+                string currentNum = i.ToString();
+                for(int j = 0; j < currentNum.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentNum[j].ToString());
+                    if(currentDigit == 0)
+                    {
+                        break;
+                    }
+                    if(n % currentDigit == 0)
+                    {
+                        count++;
+                    }
+                }
+                if(count == 4)
+                {
+                    specialNum = i;
+                    Console.Write(specialNum + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
