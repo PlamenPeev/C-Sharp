a61f1d9b51d67728239094ef5e83bd8d8b9248c3
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-02 23:06:48 +0200

    Add files via upload

---
+++ 04.Loops While and For Loops._Nested Loops - Lab/04.2.Simple_Loops_exercise/01. NumbersEndingIn7/9. MovingOut/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _9._MovingOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int availablePlace = width * length * height;
+
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Done")
+                {
+                    break;
+                }
+                
+                 int boxes = int.Parse(input);
+                 availablePlace -= boxes;
+
+                if (availablePlace < 0)
+                {
+                    break;
+                }
+
+            }
+
+
+            if (availablePlace >= 0)
+            {
+                Console.WriteLine($"{availablePlace} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(availablePlace)} Cubic meters more.");
+            }
+        }
+    }
+}
