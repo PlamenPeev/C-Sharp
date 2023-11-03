8be9b68423324d42a0cd20a004db347566936aab
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-03 18:47:52 +0200

    Add files via upload

---
+++ 6.2.List-Lab/01.ChangeList/02. List of Products/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+            for(int i = 0; i < n; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+            int count = 1;
+            foreach(string product in products) { 
+
+                Console.WriteLine(count + "." + product);
+                count++;
+            }
+        }
+    }
+}
