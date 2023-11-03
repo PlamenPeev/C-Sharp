ec09fa6d12b11f9565bcd13ee8c78daa31c118b6
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-03 18:48:10 +0200

    Add files via upload

---
+++ 6.2.List-Lab/01.ChangeList/03. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> positiveNums = new List<int>();
+
+            for(int index = 0; index <= numbers.Count - 1; index++)
+            {
+                if (numbers[index] > 0) {
+                    positiveNums.Add(numbers[index]);
+                }
+            }
+
+
+
+            positiveNums.Reverse();
+            if(positiveNums.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                foreach (int number in positiveNums)
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+    }
+}
