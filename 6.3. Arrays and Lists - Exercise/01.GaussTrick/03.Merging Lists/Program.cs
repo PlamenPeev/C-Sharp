5908de0ebc9b105f3262fd1e66e955fc2d7316c6
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-06 18:29:15 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/03.Merging Lists/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03.Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> second = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            int length = Math.Min(first.Count, second.Count);
+
+            List<int> mergingList = new List<int>();
+
+            for(int i = 0; i < length; i++)
+            {
+                mergingList.Add(first[0 + i]);
+                mergingList.Add(second[0 + i]);
+            }
+
+               
+
+            if(first.Count > second.Count)
+            {
+                for(int i = length; i < first.Count;i++)
+                {
+                    mergingList.Add(first[i]);
+                }
+            }
+            else
+            {
+                for (int i = length; i < second.Count; i++)
+                {
+                    mergingList.Add(second[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", mergingList));
+
+
+        }
+    }
+}
