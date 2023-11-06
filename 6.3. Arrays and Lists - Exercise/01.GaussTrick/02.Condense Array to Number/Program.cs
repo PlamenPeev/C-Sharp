5b8bfdee384ba92f15ddfb8a8b5cc9b40d34d40d
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-06 18:28:45 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/02.Condense Array to Number/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            
+
+            while (numbers.Count > 1)
+            {
+                List<int> output = new List<int>();
+                for(int i = 0; i < numbers.Count - 1; i++)
+                {
+                    int num1 = numbers[i];
+                    int num2 = numbers[i + 1];
+                    int currentNum = num1 + num2;
+                    output.Add(currentNum);
+                }
+                numbers = output;
+                
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
