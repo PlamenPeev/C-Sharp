4ff2d3f239e7379f1ff12de93b09ff2748a20931
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-07 21:54:58 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/05. Bomb Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string[] input = Console.ReadLine().Split(" ");
+            int bombNumber = int.Parse(input[0]);
+            int bombPower = int.Parse(input[1]);
+
+            int sum = 0;
+
+            while(numbers.Contains(bombNumber))
+            {
+                int indexBomb = numbers.IndexOf(bombNumber);
+
+                int left = Math.Max(0, indexBomb - bombPower);
+                int right = Math.Min(numbers.Count - 1, indexBomb + bombPower);
+
+                for (int i = right; i >= left; i--)
+                {
+                    numbers.RemoveAt(i);
+                }
+
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
