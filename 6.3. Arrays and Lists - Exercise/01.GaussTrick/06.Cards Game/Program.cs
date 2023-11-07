2bee0eee0ad8be3d76902508736da88f8c94b4c1
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-07 21:55:11 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/06.Cards Game/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace _06.Cards_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> second = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            while(first.Count > 0 && second.Count > 0)
+            {
+               
+                
+                    if (first[0] == second[0])
+                    {
+                    first.Remove(first[0]);
+                    second.Remove(second[0]);
+                    continue;
+                    }
+
+                     if (first[0] > second[0])
+                     {
+                        first.Add(first[0]);
+                        first.Add(second[0]);
+
+                    first.Remove(first[0]);
+                    second.Remove(second[0]);
+                    continue;
+                     }
+
+                     if (first[0] < second[0])
+                    {
+                        second.Add(second[0]);
+                        second.Add(first[0]);
+
+                    first.Remove(first[0]);
+                    second.Remove(second[0]);
+                    continue;
+                     }
+            }
+                int sumFirst = first.Sum();
+                int sumSecond = second.Sum();
+
+                if (sumFirst > 0)
+                {
+                    Console.WriteLine($"First player wins! Sum: {sumFirst}");
+                }
+                else 
+                {
+                    Console.WriteLine($"Second player wins! Sum: {sumSecond}");
+                }
+            
+        }
+    }
+}
