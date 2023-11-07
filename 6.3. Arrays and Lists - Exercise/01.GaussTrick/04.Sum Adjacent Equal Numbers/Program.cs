956e3fe1aa40aa48d71dbbd0d250f84fb5f08dc3
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-07 21:54:46 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/04.Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04.Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            int count = 0;
+            for(int i  = count; i < numbers.Count; i++)
+            {
+                
+                if (i == numbers.Count - 1) 
+                {
+                    break;
+                }
+                if (numbers[i] == numbers[i + 1])
+                {
+                    int sum = numbers[i] + numbers[i + 1];
+                    numbers[i] = sum;
+                    numbers.Remove(numbers[i + 1]);
+                    count = 0;
+                    i = -1;
+                    
+                }
+                
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
