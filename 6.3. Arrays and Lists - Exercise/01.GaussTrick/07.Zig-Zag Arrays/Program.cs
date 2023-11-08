6a4b89709570fad17a27c4dca6f12be667c2e503
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-08 21:21:59 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/07.Zig-Zag Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _07.Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = new List<int>();
+            List<int> second = new List<int>();
+
+            int num = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < num; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                int firstNum = int.Parse(input[0]);
+                int secondNum = int.Parse(input[1]);
+
+                if(i % 2 == 0)
+                {
+                    first.Add(firstNum);
+                    second.Add(secondNum);
+                }
+                else
+                {
+                    first.Add(secondNum);
+                    second.Add(firstNum);
+                }
+            }
+            Console.WriteLine(string.Join(" ", first));
+            Console.WriteLine(string.Join(" ", second));
+        }
+    }
+}
