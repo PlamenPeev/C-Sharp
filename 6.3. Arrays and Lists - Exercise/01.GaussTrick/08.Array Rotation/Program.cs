1f2102e8301af94d5957c63878a5511bc305756d
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-08 21:22:13 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/08.Array Rotation/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _08.Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                int currentNum = numbers[0];
+                numbers.Add(currentNum);
+                numbers.Remove(currentNum);
+                n--;
+            }
+            
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
