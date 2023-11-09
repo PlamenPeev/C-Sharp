b37f8723cfe505a24add8767f14b0f7f546669b4
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-09 21:16:27 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/09.MaxEqualElements/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _09.MaxEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+           
+            int maxLength = 1;
+            int currentLength = 1;
+            int longestElement = numbers[0];
+            int currentElement = numbers[0];
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber == numbers[i - 1])
+                {
+                    currentLength++;
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                        longestElement = currentElement;
+                    }
+                }
+                else
+                {
+                    currentLength = 1;
+                    currentElement = currentNumber;
+                }
+            }
+
+            List<int> longestSequence = new List<int>();
+           
+            for (int i = 0; i < maxLength; i++)
+            {
+                longestSequence.Add(longestElement);
+                
+            }
+
+            
+            Console.WriteLine(string.Join(" ", longestSequence));
+        }
+    }
+}
