0ee95541d245deb21ee66c9ea64be59e593114c3
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-09 21:16:40 +0200

    Add files via upload

---
+++ 6.3. Arrays and Lists - Exercise/01.GaussTrick/10.Top Integers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _10.Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            List<int> topIntegers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                bool isTop = true;
+                int currentNumber = numbers[i];
+
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (currentNumber <= numbers[j])
+                    {
+                        isTop = false;
+                        break;
+                    }
+                }
+
+                if (isTop)
+                {
+                    topIntegers.Add(currentNumber);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", topIntegers));
+        }
+    }
+}
