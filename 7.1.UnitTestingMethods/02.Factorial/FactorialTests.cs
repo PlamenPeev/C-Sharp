f6cc070ad0b6fb3301a538e52fae074efe1cc695
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-12 19:31:36 +0200

    Update FactorialTests.cs

--- 7.1.UnitTestingMethods/02.Factorial/FactorialTests.cs
+++ 7.1.UnitTestingMethods/02.Factorial/FactorialTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
+using System;
 
 namespace TestApp.UnitTests;
 
@@ -7,18 +8,43 @@ public class FactorialTests
     [Test]
     public void Test_CalculateFactorial_InputZero_ReturnsOne()
     {
-        // TODO: Write your test here...
+        // Arrange
+        int n = 0;
+
+        // Act
+        int result = Factorial.CalculateFactorial(n);
+
+        // Assert
+        Assert.AreEqual(1, result);
     }
 
     [Test]
     public void Test_CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
     {
-        // TODO: Write your test here...
+        // Arrange
+        int n = 5;
+
+        // Act
+        int result = Factorial.CalculateFactorial(n);
+
+        // Assert
+        Assert.AreEqual(120, result);
     }
 
     [Test]
     public void Test_CalculateFactorial_InputNegativeNumber_ThrowsException()
     {
-        // TODO: Write your test here...
+        // Arrange
+        int n = - 10;
+
+        // Act
+        int result = Factorial.CalculateFactorial(n);
+
+
+        // Assert
+
+        Assert.That(() => Factorial.CalculateFactorial(n), Throws.ArgumentException );
+       
     }
 }
+
