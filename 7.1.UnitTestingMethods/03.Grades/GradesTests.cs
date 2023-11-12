f1eebf23671904fdd0a686648bc5aeff65d92129
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-12 19:33:32 +0200

    Update GradesTests.cs

--- 7.1.UnitTestingMethods/03.Grades/GradesTests.cs
+++ 7.1.UnitTestingMethods/03.Grades/GradesTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 namespace TestApp.UnitTests;
 
@@ -7,17 +7,21 @@ public class GradesTests
     public void Test_GradeAsWords_ReturnsCorrectString(double grade, string expected)
     {
         // Arrange
+        double grades = 3.10;
+        string expect = "Average";
 
         // Act
-        string actual = Grades.GradeAsWords(grade);
+        string result = Grades.GradeAsWords(grades);
 
         // Assert
-        Assert.AreEqual(expected, actual);
+        Assert.AreEqual(expected, result);
     }
 
     public void Test_GradeAsWords_ReturnsCorrectString_EdgeCases(double grade, string expected)
     {
         // Arrange
+        double grades = 4.50;
+        string expect = "Very Good";
 
         // Act
         string actual = Grades.GradeAsWords(grade);
@@ -26,3 +30,4 @@ public class GradesTests
         Assert.AreEqual(expected, actual);
     }
 }
+
