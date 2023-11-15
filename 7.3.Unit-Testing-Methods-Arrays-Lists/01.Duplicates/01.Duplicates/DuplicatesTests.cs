f16e21cb443b2c00a0d6ef52c48e5d132e0b7469
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-15 18:33:42 +0200

    Add files via upload

---
+++ 7.3.Unit-Testing-Methods-Arrays-Lists/01.Duplicates/01.Duplicates/DuplicatesTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class DuplicatesTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_RemoveDuplicates_EmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        int[] numbers = Array.Empty<int>();
+
+        // Act
+        int[] result = Duplicates.RemoveDuplicates(numbers);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    
+    [Test]
+    public void Test_RemoveDuplicates_NoDuplicates_ReturnsOriginalArray()
+    {
+        // Arrange
+        int[] numbers = new int[] {1, 2, 3, 4};
+
+        // Act
+        int[] result = Duplicates.RemoveDuplicates(numbers);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new int[] {1, 2, 3, 4}));
+    }
+
+    [Test]
+    public void Test_RemoveDuplicates_SomeDuplicates_ReturnsUniqueArray()
+    {
+        // Arrange
+        int[] numbers = new int[] { 1, 2, 3, 4, 2, 5, 4 };
+
+        // Act
+        int[] result = Duplicates.RemoveDuplicates(numbers);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new int[] { 1, 2, 3, 4, 5 }));
+    }
+
+    [Test]
+    public void Test_RemoveDuplicates_AllDuplicates_ReturnsSingleElementArray()
+    {
+        // Arrange
+        int[] numbers = new int[] { 1, 2, 1, 2, 4, 4 };
+
+        // Act
+        int[] result = Duplicates.RemoveDuplicates(numbers);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new int[] { 1, 2, 4 }));
+    }
+}
