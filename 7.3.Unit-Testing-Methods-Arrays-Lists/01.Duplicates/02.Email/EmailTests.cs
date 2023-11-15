363002a644cfd1d199621e96aaba43cf5e77d9fc
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-15 18:34:07 +0200

    Add files via upload

---
+++ 7.3.Unit-Testing-Methods-Arrays-Lists/01.Duplicates/02.Email/EmailTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailTests
+{
+    
+    [Test]
+    public void Test_IsValidEmail_ValidEmail()
+    {
+        // Arrange
+        string validEmail = "test@example.com";
+
+        // Act
+        bool isValid = Email.IsValidEmail(validEmail);
+
+        // Assert
+        Assert.IsTrue(isValid);
+    }
+
+    [Test]
+    public void Test_IsValidEmail_InvalidEmail()
+    {
+        // Arrange
+        string validEmail = "test_test";
+
+        // Act
+        bool isInvalid = Email.IsValidEmail(validEmail);
+
+        // Assert
+        Assert.IsFalse(isInvalid);  
+    }
+
+
+    [Test]
+    public void Test_IsValidEmail_NullInput()
+    {
+        // Arrange
+        string validEmail = " ";
+
+        // Act
+        bool isInvalid = Email.IsValidEmail(validEmail);
+
+        // Assert
+        Assert.IsFalse(isInvalid);
+    }
+}
