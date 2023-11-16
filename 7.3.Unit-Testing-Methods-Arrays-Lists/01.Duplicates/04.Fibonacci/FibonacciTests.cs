85c04cc683970e2ad3b3d320ae754a346088ff67
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-16 18:50:59 +0200

    Add files via upload

---
+++ 7.3.Unit-Testing-Methods-Arrays-Lists/01.Duplicates/04.Fibonacci/FibonacciTests.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class FibonacciTests
+{
+    [Test]
+    public void Test_CalculateFibonacci_NegativeInput()
+    {
+        // Arrange
+        int n = -1;
+
+        // Act & Assert
+        Assert.That(() => Fibonacci.CalculateFibonacci(n), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_CalculateFibonacci_ZeroInput()
+    {
+        // Arrange
+        int n = 0;
+
+        //Act
+        int result = Fibonacci.CalculateFibonacci(n);
+
+
+        //Assert
+        Assert.IsTrue(result == 0);
+    }
+
+    [Test]
+    public void Test_CalculateFibonacci_PositiveInput()
+    {
+        // Arrange
+        int n = 7;
+
+        //Act
+        int result = Fibonacci.CalculateFibonacci(n);
+
+
+        //Assert
+        Assert.IsTrue((int)result == 13);
+    }
+}
