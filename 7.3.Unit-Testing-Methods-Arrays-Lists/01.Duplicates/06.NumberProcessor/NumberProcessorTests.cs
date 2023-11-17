980358926e1821f82384207bde22f13f9651ff9c
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-17 18:58:32 +0200

    Add files via upload

---
+++ 7.3.Unit-Testing-Methods-Arrays-Lists/01.Duplicates/06.NumberProcessor/NumberProcessorTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class NumberProcessorTests
+{
+    [Test]
+    public void Test_ProcessNumbers_SquareEvenNumbers()
+    {
+        // Arrange
+        List<int> input = new() { 2, 4, 6 };
+        List<double> expected = new() { 4, 16, 36 };
+
+        // Act
+        List<double> actual = NumberProcessor.ProcessNumbers(input);
+
+        // Assert
+        CollectionAssert.AreEqual(expected, actual);
+    }
+
+    
+    [Test]
+    public void Test_ProcessNumbers_SquareRootOddNumbers()
+    {
+        // Arrange
+        List<int> input = new() { 1, 9, 25 };
+        List<double> expected = new() { 1, 3, 5};
+
+        // Act
+        List<double> actual = NumberProcessor.ProcessNumbers(input);
+
+        // Assert
+        CollectionAssert.AreEqual(expected, actual);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_ProcessNumbers_HandleZero()
+    {
+        // Arrange
+        List<int> input = new() { 0 };
+        List<int> expected = new() { 0 };
+
+        // Act
+        List<double> actual = NumberProcessor.ProcessNumbers(input);
+
+        // Assert
+        CollectionAssert.AreEqual(expected, actual);
+
+    }
+
+    [Test]
+    public void Test_ProcessNumbers_HandleNegativeNumbers()
+    {
+        // Arrange
+        List<int> input = new() { -23 };
+
+
+        // Act & Assert
+        Assert.That(() => { NumberProcessor.ProcessNumbers(input); }, Throws.ArgumentException);
+
+        
+    }
+
+    [Test]
+    public void Test_ProcessNumbers_EmptyInput()
+    {
+        // Arrange
+        List<int> input = new() { };
+        List<int> expected = new() { };
+
+        // Act
+        List<double> actual = NumberProcessor.ProcessNumbers(input);
+
+        // Assert
+        Assert.That(actual, Is.Empty);
+    }
+}
