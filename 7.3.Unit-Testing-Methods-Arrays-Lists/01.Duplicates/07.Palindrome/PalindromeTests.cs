d3336e244b64f4d400b2053c8bfc727e9c773aa8
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-17 18:58:49 +0200

    Add files via upload

---
+++ 7.3.Unit-Testing-Methods-Arrays-Lists/01.Duplicates/07.Palindrome/PalindromeTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class PalindromeTests
+{
+   
+    [Test]
+    public void Test_IsPalindrome_ValidPalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> input = new() { "alaala" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    
+    [Test]
+    public void Test_IsPalindrome_EmptyList_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new();
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_SingleWord_ReturnsTrue()
+    {
+        // Arrange
+        List<string> input = new() { "bananab" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_NonPalindrome_ReturnsFalse()
+    {
+        // Arrange
+        List<string> input = new() { "word" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_MixedCasePalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> input = new() { "PalaYalaP" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(input);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+}
