16b9defacfde1718b3330cb07cf1c3498d2f465b
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-18 10:14:22 +0200

    Add files via upload

---
+++ 7.3.Unit-Testing-Methods-Arrays-Lists/01.Duplicates/09.PrimeFactor/PrimeFactorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class PrimeFactorTests
+{
+    [Test]
+    public void Test_FindLargestPrimeFactor_NumberLowerThanTwo()
+    {
+        // Arrange
+        long num = 1;
+
+
+        // Act & Assert
+        Assert.That(() => { PrimeFactor.FindLargestPrimeFactor(num); }, Throws.ArgumentException);
+    }
+
+
+    [Test]
+    public void Test_FindLargestPrimeFactor_PrimeNumber()
+    {
+         // Arrange
+         long num = 29;
+
+        // Act
+        long result = PrimeFactor.FindLargestPrimeFactor(num);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(29));
+
+    }
+
+    [Test]
+    public void Test_FindLargestPrimeFactor_LargeNumber()
+    {
+        // Arrange
+        long num = 97;
+
+        // Act
+        long result = PrimeFactor.FindLargestPrimeFactor(num);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(97));
+    }
+}
