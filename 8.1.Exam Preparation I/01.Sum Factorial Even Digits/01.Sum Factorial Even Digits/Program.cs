3e3ffb99ce3f0872b5b277a2212e8e45fcb1a136
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-20 22:12:17 +0200

    Add files via upload

---
+++ 8.1.Exam Preparation I/01.Sum Factorial Even Digits/01.Sum Factorial Even Digits/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Sum_Factorial_Even_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int sum = 0;
+
+            for(int i = 0; i < number.Length;i++)
+            {
+                int currentDigit = int.Parse(number[i].ToString());
+
+                if(currentDigit % 2 == 0)
+                {
+                    int f1 = getFactorial(currentDigit);
+                    sum += f1;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static int getFactorial(int num)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
