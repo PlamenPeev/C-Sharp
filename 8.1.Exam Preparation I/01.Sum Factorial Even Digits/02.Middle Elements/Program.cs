115957334fc27b5290198f7554156fa3c82b137e
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-20 22:12:37 +0200

    Add files via upload

---
+++ 8.1.Exam Preparation I/01.Sum Factorial Even Digits/02.Middle Elements/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02.Middle_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            double num1 = numbers[numbers.Length / 2 - 1];
+            double num2 = numbers[numbers.Length / 2];
+            
+            double averageSum = (num1 + num2) / 2;
+            Console.WriteLine($"{averageSum:F2}");
+        }
+    }
+}
