8a271a085b092d577fdb60b2040a4b79cfa167f4
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-23 22:55:28 +0200

    Add files via upload

---
+++ 8.2.Exam Preparation II/01. Magic Numbers/02. Min _Max Values/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02._Min__Max_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+
+            for(int i = 0; i < num; i++)
+            {
+                int currentNum = arr[i];
+
+                if(currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+
+                if (currentNum < minNum)
+                {
+                    minNum = currentNum;
+                }
+            }
+
+            Console.WriteLine(maxNum);
+            Console.WriteLine(minNum);
+
+        }
+    }
+}
