996ccb30f01a0f8325d05ffc4028937ad3d0c182
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-25 17:27:41 +0200

    Add files via upload

---
+++ 8.3. Exam Preparation III/1. English Name of Each Digit/2. Average Last Elements/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2._Average_Last_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            for(int i = numbers.Length -1; i >= n; i--)
+            {
+                sum += numbers[i];
+            }
+            double average = sum / n;
+            Console.WriteLine($"{average:f2}");
+        }
+    }
+}
