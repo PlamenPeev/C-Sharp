2fc2942c5484186a0c2060accab096b3b65debd0
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-25 17:27:21 +0200

    Add files via upload

---
+++ 8.3. Exam Preparation III/1. English Name of Each Digit/1. English Name of Each Digit/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _1._English_Name_of_Each_Digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            while (number > 0)
+            {
+                int currentNum = number % 10;
+
+                switch (currentNum)
+                {
+                    case 1: Console.WriteLine("one");break;
+                    case 2: Console.WriteLine("two"); break;
+                    case 3: Console.WriteLine("three"); break;
+                    case 4: Console.WriteLine("four"); break;
+                    case 5: Console.WriteLine("five"); break;
+                    case 6: Console.WriteLine("six"); break;
+                    case 7: Console.WriteLine("seven"); break;
+                    case 8: Console.WriteLine("eight"); break;
+                    case 9: Console.WriteLine("nine"); break;
+                  
+                }
+
+                number = number / 10;
+            }
+        }
+    }
+}
