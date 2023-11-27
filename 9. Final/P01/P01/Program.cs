7a186b0cdef7b90fbd1d66c1525a5848ce32f09c
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-27 18:52:12 +0200

    Add files via upload

---
+++ 9. Final/P01/P01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                sum += grade;
+            }
+
+            double average = sum / n;
+
+            Console.WriteLine($"{average:f2}");
+        }
+    }
+}
