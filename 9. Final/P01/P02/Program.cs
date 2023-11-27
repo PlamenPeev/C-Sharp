d12acb3dc7ddf5ca9b6e80b35a81da68ba4e1a8b
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-27 18:52:40 +0200

    Add files via upload

---
+++ 9. Final/P01/P02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+
+
+            for(int i = 1; i <= rotations; i++)
+            {
+                int currentNum = numbers[numbers.Count - 1];
+                numbers.Insert(0, currentNum);
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
