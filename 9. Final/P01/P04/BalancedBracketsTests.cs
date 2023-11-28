796090015e58a0935e328e4e7926de59a2c7a137
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-28 18:50:25 +0200

    Add files via upload

---
+++ 9. Final/P01/P04/BalancedBracketsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class BalancedBracketsTests
+{
+    [Test]
+    public void Test_IsBalanced_EmptyInput_ShouldReturnTrue()
+    {
+        // Arrange
+        string[] input = { };
+
+        //Act
+        bool isValid = BalancedBrackets.IsBalanced(input);
+
+        //Assert
+        Assert.IsTrue(isValid);
+
+    }
+
+    [Test]
+    public void Test_IsBalanced_BalancedBrackets_ShouldReturnTrue()
+    {
+        // Arrange
+        string[] input = { "(", ")", "(", ")", "(", ")" };
+
+        //Act
+        bool isValid = BalancedBrackets.IsBalanced(input);
+
+        //Assert
+        Assert.IsTrue(isValid);
+    }
+
+    [Test]
+    public void Test_IsBalanced_UnbalancedBrackets_ShouldReturnFalse()
+    {
+        // Arrange
+        string[] input = { "(", ")", "(", ")", "(" };
+
+        //Act
+        bool isValid = BalancedBrackets.IsBalanced(input);
+
+        //Assert
+        Assert.IsFalse(isValid);
+    }
+
+    [Test]
+    public void Test_IsBalanced_MoreClosingBrackets_ShouldReturnFalse()
+    {
+        // Arrange
+        string[] input = { "(", ")", ")", "(", ")", "(", ")", ")" };
+
+        //Act
+        bool isValid = BalancedBrackets.IsBalanced(input);
+
+        //Assert
+        Assert.IsFalse(isValid);
+    }
+
+    [Test]
+    public void Test_IsBalanced_NoClosingBrackets_ShouldReturnFalse()
+    {
+        // Arrange
+        string[] input = { "(", "(", "(" };
+
+        //Act
+        bool isValid = BalancedBrackets.IsBalanced(input);
+
+        //Assert
+        Assert.IsFalse(isValid);
+    }
+}
