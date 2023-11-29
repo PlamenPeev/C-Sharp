66b4be1485dc5e5dc64d1e4f4f3bfa43665a1c26
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-29 21:06:27 +0200

    Add files via upload

---
+++ 01.Advanced_Strings and Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+
+            while(text != "end")
+            {
+                string reverseText = "";
+
+                //for(int i = text.Length - 1; i >= 0; i--)
+                //{
+                //    reverseText += text[i];
+                //}
+                
+                
+                //char[] arr = text.ToCharArray();
+                //Array.Reverse(arr);
+                //reverseText = string.Join("",arr);
+
+
+                reverseText = string.Join("", text.ToCharArray().Reverse());
+
+
+                Console.WriteLine($"{text}" + " = " + $"{reverseText}");
+
+               
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
