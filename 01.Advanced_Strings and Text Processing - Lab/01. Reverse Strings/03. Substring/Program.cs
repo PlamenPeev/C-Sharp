ccf04db880fbe24c60cd3a6fa3398d2b174b18a3
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-11-30 20:23:54 +0200

    Add files via upload

---
+++ 01.Advanced_Strings and Text Processing - Lab/01. Reverse Strings/03. Substring/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string target = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(target))
+            {
+
+                //int index = text.IndexOf(target);
+                //text = text.Remove(index,target.Length);
+
+                text = text.Replace(target, "");
+               
+            }
+            
+
+            Console.WriteLine(text);
+        }
+    }
+}
