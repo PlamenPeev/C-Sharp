4062eea6af2d1442c02219730ec10072a2d94818
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-03 11:54:59 +0200

    Add files via upload

---
+++ 03.PA_QA_UnitTesting_String_REGEX/01.EmailValidator/EmailValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailValidatorTests
+{
+
+    [TestCase("ivan.ivanov@abv.bg")]
+    [TestCase("P.peshov@gmail.com")]
+    [TestCase("s_sashov@meil.ru")]
+    public void Test_ValidEmails_ReturnsTrue(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+
+    [TestCase("ivan_A/bg.ivanov@abv.bg")]
+    [TestCase("propa.peshov.yahoo.com")]
+    [TestCase("sasho.sd.sashov@bg@org")]
+    public void Test_InvalidEmails_ReturnsFalse(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
