0c51c14a57b8efff0bdd489b3b561284279a36da
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-03 11:53:04 +0200

    Add files via upload

---
+++ 02.PA_Regular Expressions - Lab/01. Match Full Name/03. Match Dates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+
+            string pattern = @"\b(?<day>\d{2})([\/\-\.])(?<month>[A-Z]{1}[a-z]{2})\2(?<year>\d{4})\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(text);
+
+
+            foreach (Match match in matches)
+            {
+              Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+
+            }
+        }
+    }
+}
