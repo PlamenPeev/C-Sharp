8111fccc44b07b1f5701789ceadf52a98a3732e7
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-05 18:44:46 +0200

    Add files via upload

---
+++ 03.PA_QA_UnitTesting_String_REGEX/01.EmailValidator/06.Patterns/PatternTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class PatternTests
+{
+
+    [TestCase("abc", 1, "aBc")]
+    [TestCase("AbC", 3, "aBcaBcaBc")]
+    [TestCase("ABC", 2, "aBcaBc")]
+    public void Test_GeneratePatternedString_ValidInput_ReturnsExpectedResult(string input, 
+        int repetitionFactor, string expected)
+    {
+        // Arrange
+
+        // Act
+        string result = Pattern.GeneratePatternedString(input, repetitionFactor);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_EmptyInput_ThrowsArgumentException()
+    {
+        // Arrange
+        string input = string.Empty;
+        int repetitionFactor = 1;
+        
+
+        // Act
+        //string result = Pattern.GeneratePatternedString(input, repetitionFactor);
+
+        // Assert
+        Assert.That(() => Pattern.GeneratePatternedString(input, repetitionFactor), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_NegativeRepetitionFactor_ThrowsArgumentException()
+    {
+        // Arrange
+        string input = "hello";
+        int repetitionFactor = -1;
+
+
+        // Act
+        //string result = Pattern.GeneratePatternedString(input, repetitionFactor);
+
+        // Assert
+        Assert.That(() => Pattern.GeneratePatternedString(input, repetitionFactor), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_ZeroRepetitionFactor_ThrowsArgumentException()
+    {
+        // Arrange
+        string input = "hello";
+        int repetitionFactor = 0;
+
+
+        // Act
+        //string result = Pattern.GeneratePatternedString(input, repetitionFactor);
+
+        // Assert
+        Assert.That(() => Pattern.GeneratePatternedString(input, repetitionFactor), Throws.ArgumentException);
+    }
+}
