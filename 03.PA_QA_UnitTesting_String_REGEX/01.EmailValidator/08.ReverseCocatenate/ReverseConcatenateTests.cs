119cfeca89bd5642354722d5a3096d586dd79fdb
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-06 22:14:37 +0200

    Add files via upload

---
+++ 03.PA_QA_UnitTesting_String_REGEX/01.EmailValidator/08.ReverseCocatenate/ReverseConcatenateTests.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Linq;
+
+namespace TestApp.UnitTests;
+
+public class ReverseConcatenateTests
+{
+    
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_EmptyInput_ReturnsEmptyString()
+    {
+        // Arrange
+        string[]? input = { };
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        CollectionAssert.AreEqual(result, "");
+    }
+
+    
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_SingleString_ReturnsSameString()
+    {
+        // Arrange
+        string[]? input = {"abc"};
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        CollectionAssert.AreEqual(result, "abc");
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_MultipleStrings_ReturnsReversedConcatenatedString()
+    {
+        // Arrange
+        string[]? input = { "Hello", "my", "dog" };
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        CollectionAssert.AreEqual(result, "dogmyHello");
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_NullInput_ReturnsEmptyString()
+    {
+        string[]? input = null;
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        CollectionAssert.AreEqual(result, "");
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_WhitespaceInput_ReturnsConcatenatedString()
+    {
+        // Arrange
+        string[]? input = { " Hello", " my", "dog" };
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        CollectionAssert.AreEqual(result, "dog my Hello");
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_LargeInput_ReturnsReversedConcatenatedString()
+    {
+        // Arrange
+        string[]? input = { "Hello", "my", "dog", "Hello", "my", "dog", "Hello", "my", "dog" };
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        CollectionAssert.AreEqual(result, "dogmyHellodogmyHellodogmyHello");
+    }
+}
