f7d94311f37dcc01bdddcf492b6694f2c62e6a5c
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-08 21:35:28 +0200

    Add files via upload

---
+++ 04.PA_Dictionaries, Lambda and LINQ - Lab/01. Count Real Numbers/01. Count Real Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            var counts = new SortedDictionary<int, int>();
+
+            foreach ( int number in numbers )
+            {
+                if ( counts.ContainsKey(number) )
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+
+            foreach(var num in counts)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+
+        }
+    }
+}
