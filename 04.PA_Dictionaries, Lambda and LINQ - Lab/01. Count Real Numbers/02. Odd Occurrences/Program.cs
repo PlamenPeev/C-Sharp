bd978b4d05fed101b8eaf5e27751e929f53ffba6
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-09 14:14:09 +0200

    Add files via upload

---
+++ 04.PA_Dictionaries, Lambda and LINQ - Lab/01. Count Real Numbers/02. Odd Occurrences/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ").Select(w => w.ToLower()).ToArray();
+
+            var counts = new Dictionary<string, int>();
+
+          
+            foreach (var word in words)
+            {
+                
+                if (counts.ContainsKey(word))
+                {
+                    counts[word]++;
+                }
+                else
+                {
+                    counts[word] = 1;
+                }
+            }
+
+            foreach (var word in counts)
+            {
+                if(word.Value % 2 != 0)
+                {
+                    Console.Write(word.Key + " ");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
