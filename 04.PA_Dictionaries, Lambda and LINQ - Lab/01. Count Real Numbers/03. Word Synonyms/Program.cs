5bbd2f0ed7007dbbe12e5b14662c12e0fe22d907
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-09 14:14:25 +0200

    Add files via upload

---
+++ 04.PA_Dictionaries, Lambda and LINQ - Lab/01. Count Real Numbers/03. Word Synonyms/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var result = new Dictionary<string, List<string>>();
+            
+            for(int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+
+                if(!result.ContainsKey(word))
+                {
+                    result.Add(word, new List<string>());
+                    result[word].Add(synonim);
+                }
+                else
+                {
+                    result[word].Add(synonim);
+                }
+            }
+
+            foreach(var word in result)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}"); 
+                
+            }
+            
+        }
+    }
+}
