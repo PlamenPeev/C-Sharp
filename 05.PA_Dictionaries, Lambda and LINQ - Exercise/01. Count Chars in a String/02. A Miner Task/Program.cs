6dd95f06c21ac08cefd27a47491041585fb473af
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-10 20:44:26 +0200

    Add files via upload

---
+++ 05.PA_Dictionaries, Lambda and LINQ - Exercise/01. Count Chars in a String/02. A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, int> minerTask = new Dictionary<string, int>();
+
+
+            while(input != "stop")
+            {
+                string resource = input;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if(!minerTask.ContainsKey(resource))
+                {
+                    minerTask.Add(resource, quantity);
+                }
+                else
+                {
+                    minerTask[resource] += quantity;
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            foreach(var task in minerTask)
+            {
+                Console.WriteLine($"{task.Key} -> {task.Value}");
+            }
+        }
+    }
+}
