c478dd6ace094c5fe2f39ab1a5cc978adf6ba8da
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-10 20:43:43 +0200

    Add files via upload

---
+++ 05.PA_Dictionaries, Lambda and LINQ - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char,int> countChars = new Dictionary<char,int>();
+
+            for(int i = 0;i < input.Length;i++)
+            {
+                char symbol = char.Parse(input[i].ToString());
+                if (symbol == ' ')
+                {
+                    continue;
+                }
+
+                if (!countChars.ContainsKey(symbol))
+                {
+                    countChars.Add(symbol, 1);
+                }
+                else
+                {
+                    countChars[symbol]++;
+                }
+                
+            }
+
+           foreach(var symbol in countChars)
+            {
+                Console.WriteLine($"{symbol.Key} -> {symbol.Value}");
+            }
+        }
+    }
+}
