a7f057c8fd1f334d8e556b7862e45fde253a82ff
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-11 19:01:28 +0200

    Add files via upload

---
+++ 05.PA_Dictionaries, Lambda and LINQ - Exercise/01. Count Chars in a String/06. Company Users/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _06._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,List<string>> compnies = new Dictionary<string,List<string>>();
+
+            string input = Console.ReadLine();
+
+            while(input != "End")
+            {
+                string company = input.Split(" -> ")[0];
+                string employeeId = input.Split(" -> ")[1];
+
+                if(!compnies.ContainsKey(company))
+                {
+                    compnies.Add(company, new List<string>());
+                   
+                }
+               
+                    if (!compnies[company].Contains(employeeId))
+                    {
+                        compnies[company].Add(employeeId);
+                    }
+                   
+
+                input = Console.ReadLine();
+            }
+
+            foreach(var company in compnies)
+            {
+                Console.WriteLine(company.Key);
+                foreach(var employee in company.Value)
+                {
+                   Console.WriteLine($"-- {employee}");
+                }
+                
+            }
+        }
+    }
+}
