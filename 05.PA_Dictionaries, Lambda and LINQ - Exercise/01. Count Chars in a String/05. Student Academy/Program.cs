ff26aeadaa7f67fb6c86edcd43b5b17a1bbdc19c
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-11 19:01:11 +0200

    Add files via upload

---
+++ 05.PA_Dictionaries, Lambda and LINQ - Exercise/01. Count Chars in a String/05. Student Academy/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Linq;
+namespace _05._Student_Academy
+    
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string,List<double>> students = new Dictionary<string,List<double>>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string student = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(student))
+                {
+                    students.Add(student, new List<double>());
+                    
+                }
+                students[student].Add(grade);
+            }
+
+            var averageStudents = students.Where(s => s.Value.Average() >= 4.50);
+            foreach(var student in averageStudents)
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            }
+        }
+    }
+}
