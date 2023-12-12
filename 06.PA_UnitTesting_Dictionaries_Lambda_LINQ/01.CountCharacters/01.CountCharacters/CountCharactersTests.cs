901f7b2ca116fa04cd8e91a49da3d347b50ecbf0
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-12 20:58:40 +0200

    Add files via upload

---
+++ 06.PA_UnitTesting_Dictionaries_Lambda_LINQ/01.CountCharacters/01.CountCharacters/CountCharactersTests.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.Tests;
+
+public class CountCharactersTests
+{
+    [Test]
+    public void Test_Count_WithEmptyList_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new() { };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    
+    [Test]
+    public void Test_Count_WithNoCharacters_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new() {""};
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleCharacter_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "b" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("b -> 1"));
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "b", "c", "cd", "eff" };
+        StringBuilder sb = new();
+        sb.AppendLine("b -> 1");
+        sb.AppendLine("c -> 2");
+        sb.AppendLine("d -> 1");
+        sb.AppendLine("e -> 1");
+        sb.AppendLine("f -> 2");
+
+        string expected = sb.ToString().Trim();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithSpecialCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "b", "&", "&d", "*ff" };
+        StringBuilder sb = new();
+        sb.AppendLine("b -> 1");
+        sb.AppendLine("& -> 2");
+        sb.AppendLine("d -> 1");
+        sb.AppendLine("* -> 1");
+        sb.AppendLine("f -> 2");
+
+        string expected = sb.ToString().Trim();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithNumbers_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "1", "2", "12", "33" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("1 -> 2");
+        sb.AppendLine("2 -> 2");
+        sb.AppendLine("3 -> 2");
+
+        string expected = sb.ToString().Trim();
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
