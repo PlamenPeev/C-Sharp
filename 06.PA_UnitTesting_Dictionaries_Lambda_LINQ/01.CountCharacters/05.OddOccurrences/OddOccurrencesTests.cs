28433812872e1fac8970c4ebce18acc25f8503a9
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-15 18:17:26 +0200

    Add files via upload

---
+++ 06.PA_UnitTesting_Dictionaries_Lambda_LINQ/01.CountCharacters/05.OddOccurrences/OddOccurrencesTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.Tests;
+
+public class OddOccurrencesTests
+{
+    [Test]
+    public void Test_FindOdd_WithEmptyArray_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] input = new string[] {};
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    
+    [Test]
+    public void Test_FindOdd_WithNoOddOccurrences_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] input = new string[] { "a","a"};
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_FindOdd_WithSingleOddOccurrence_ShouldReturnTheOddWord()
+    {
+        // Arrange
+        string[] input = new string[] { "a" };
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("a"));
+    }
+
+    [Test]
+    public void Test_FindOdd_WithMultipleOddOccurrences_ShouldReturnAllOddWords()
+    {
+        // Arrange
+        string[] input = new string[] { "a", "ab", "abc" };
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("a ab abc"));
+    }
+
+    [Test]
+    public void Test_FindOdd_WithMixedCaseWords_ShouldBeCaseInsensitive()
+    {
+        // Arrange
+        string[] input = new string[] { "a", "aAb", "aBc", "aAb", "aBc", "AAb", "ABc" };
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("a aab abc"));
+    }
+}
