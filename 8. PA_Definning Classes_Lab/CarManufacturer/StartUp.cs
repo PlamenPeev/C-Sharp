7ce8a5558dfd214ff76f1e51b602a17e784af165
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-20 21:12:30 +0200

    Add files via upload

---
+++ 8. PA_Definning Classes_Lab/CarManufacturer/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // 01.Car ----------------------------------------------------------------
+            //Car car = new Car();
+            //car.Make = "VW";
+            //car.Model = "MK3";
+            //car.Year = 1992;
+            //Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+
+            //02.Car Extension -------------------------------------------------------
+            //Car car = new Car();
+            //car.Make = "VW";
+            //car.Model = "MK3";
+            //car.Year = 1992;
+            //car.FuelQuantity = 200;
+            //car.FuelConsumption = 200;
+            //car.Drive(2000);
+            //Console.WriteLine(car.WhoAmI());
+
+            //03.Car Constructors ----------------------------------------------------
+            //string make = Console.ReadLine();
+            //string model = Console.ReadLine();
+            //int year = int.Parse(Console.ReadLine());
+            //double fuelQuantity = double.Parse(Console.ReadLine());
+            //double fuelConsumption = double.Parse(Console.ReadLine());
+
+            //Car firstCar = new Car();
+            //Car secondCar = new Car(make, model, year);
+            //Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+
+            var tires = new Tire[4]
+            {
+                new Tire(1, 2.5),
+                new Tire(1, 2.1),
+                new Tire(2, 0.5),
+                new Tire(2, 2.3)
+            };
+
+            var engine = new Engine(560, 6300);
+
+            var car = new Car("Lamborgini", "Urus", 2010, 250, 9, engine, tires);
+            
+        }
+    }
+}
