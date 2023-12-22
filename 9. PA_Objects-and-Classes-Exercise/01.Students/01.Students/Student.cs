8a0b000220ff53668021a05e4ab740facdd21d5b
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-22 19:37:41 +0200

    Add files via upload

---
+++ 9. PA_Objects-and-Classes-Exercise/01.Students/01.Students/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Students
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private double grade;
+
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public double Grade { get {  return grade; } set {  grade = value; } }
+
+        public Student(string firstName, string lastName, double grade) 
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+
+        }
+
+
+    }
+}
