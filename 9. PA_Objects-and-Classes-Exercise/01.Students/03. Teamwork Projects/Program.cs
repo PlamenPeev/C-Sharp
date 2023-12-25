352215cfd1eff743055ccf43d3ac35676032699b
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-25 20:21:51 +0200

    Add files via upload

---
+++ 9. PA_Objects-and-Classes-Exercise/01.Students/03. Teamwork Projects/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _3._Teamwork_Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfTeams = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Team> teams = new Dictionary<string, Team>();
+
+            for (int i = 0; i < countOfTeams; i++)
+            {
+                string[] data = Console.ReadLine().Split("-");
+                string creator = data[0];
+                string teamName = data[1];
+
+                if (teams.Values.Any(team => team.Creator == creator))
+                {
+                    Console.WriteLine($"{creator} cannot create another team!");
+                    break;
+                }
+
+                    if (!teams.ContainsKey(teamName))
+                    {
+                            Team team = new Team(teamName, creator);
+                            teams.Add(teamName, team);
+                            Console.WriteLine($"Team {teamName} has been created by {creator}!");
+                    }
+                    else
+                    {
+                      Console.WriteLine($"Team {teamName} was already created!");
+                    }
+
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "end of assignment")
+            {
+                string[] dataMembers = input.Split("->");
+                string member = dataMembers[0];
+                string teamToJoin = dataMembers[1];
+
+                if (teams.ContainsKey(teamToJoin))
+                {
+                    if (teams[teamToJoin].Membres.Contains(member) || teams[teamToJoin].Creator.Equals(member)
+                        || teams.Values.Any(team => team.Membres.Contains(member)))
+                    {
+                        Console.WriteLine($"Member {member} cannot join team {teamToJoin}!");
+                       
+                    }
+                    else
+                    {
+                        teams[teamToJoin].Membres.Add(member);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"Team {teamToJoin} does not exist!");
+                    
+                }
+
+                input = Console.ReadLine();
+
+            }
+
+            foreach (var team in teams.Where(t => t.Value.Membres.Count > 0)
+                .OrderByDescending(t => t.Value.Membres.Count)
+                .ThenBy(t => t.Key))
+            {
+                Console.WriteLine(team.Key);
+                Console.WriteLine("- " + team.Value.Creator);
+
+                foreach (string member in team.Value.Membres.OrderBy(m => m))
+                {
+                    Console.WriteLine("-- " + member);
+                }
+            }
+
+            Console.WriteLine("Teams to disband:");
+            
+          
+            foreach(var team in teams.OrderBy(t => t.Value.Name)
+                .Where(t => t.Value.Membres.Count == 0))
+            {
+                Console.WriteLine(team.Key);
+            }
+        }
+    }
+}
---
+++ 9. PA_Objects-and-Classes-Exercise/01.Students/03. Teamwork Projects/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._Teamwork_Projects
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Creator { get; set; }
+        public List<string> Membres { get; set; }
+
+        public Team(string name, string creator)
+        {
+            Name = name;
+            Creator = creator;
+            Membres = new List<string>();
+        }
+    }
+}
