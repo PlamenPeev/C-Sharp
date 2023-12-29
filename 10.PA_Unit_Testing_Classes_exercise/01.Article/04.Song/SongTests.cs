987a167840680382d4828c41c72d7cd6a0f6bb9a
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-12-29 22:12:36 +0200

    Add files via upload

---
+++ 10.PA_Unit_Testing_Classes_exercise/01.Article/04.Song/SongTests.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+
+using System;
+using static NUnit.Framework.Constraints.Tolerance;
+
+namespace TestApp.UnitTests;
+
+public class SongTests
+{
+    private Song _song;
+
+    [SetUp]
+    public void Setup()
+    {
+        this._song = new();
+    }
+
+    
+    [Test]
+    public void Test_AddAndListSongs_ReturnsAllSongs_WhenWantedListIsAll()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = $"Song1{Environment.NewLine}Song2{Environment.NewLine}Song3";
+
+        // Act
+
+        string result = this._song.AddAndListSongs(songs, "all");
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsFilteredSongs_WhenWantedListIsSpecific()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = $"Song1{Environment.NewLine}Song3";
+
+        // Act
+
+        string result = this._song.AddAndListSongs(songs, "Pop");
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsEmptyString_WhenNoSongsMatchWantedList()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        
+
+        // Act
+
+        string result = this._song.AddAndListSongs(songs, "Folk");
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+}
