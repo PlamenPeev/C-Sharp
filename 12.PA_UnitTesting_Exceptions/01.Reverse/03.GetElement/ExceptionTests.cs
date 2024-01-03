dac09ec5483f2ec8b57ccb5ed7774b99e816864c
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2024-01-03 20:56:38 +0200

    Add files via upload

---
+++ 12.PA_UnitTesting_Exceptions/01.Reverse/03.GetElement/ExceptionTests.cs
@@ -0,0 +1,242 @@
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace TestApp.UnitTests;
+
+public class ExceptionTests
+{
+    private Exceptions _exceptions = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._exceptions = new();
+    }
+
+    //public void Test_Reverse_ValidString_ReturnsReversedString()
+    //{
+    //    Test_Reverse_ValidString_ReturnsReversedString(_exceptions);
+    //}
+
+    [Test]
+    public void Test_Reverse_ValidString_ReturnsReversedString()
+    {
+        // Arrange
+        string input = "umbrella";
+        string expected = "allerbmu";
+
+        // Act
+        string result = this._exceptions.ArgumentNullReverse(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    
+    [Test]
+    public void Test_Reverse_NullString_ThrowsArgumentNullException()
+    {
+        // Arrange
+        string input = null;
+
+        // Act & Assert
+        Assert.That(() => this._exceptions.ArgumentNullReverse(input), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_CalculateDiscount_ValidInput_ReturnsDiscountedPrice()
+    {
+        // Arrange
+        decimal totalPrice = 100;
+        decimal discount = 10;
+        decimal expected = totalPrice - totalPrice * discount / 100;
+
+        //Act
+        decimal result = this._exceptions.ArgumentCalculateDiscount(totalPrice, discount);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    
+    [Test]
+    public void Test_CalculateDiscount_NegativeDiscount_ThrowsArgumentException()
+    {
+        // Arrange
+        decimal totalPrice = 100;
+        decimal discount = -10;
+       
+
+        // Act & Assert
+        Assert.That(() => this._exceptions.ArgumentCalculateDiscount(totalPrice, discount), Throws.ArgumentException);
+    }
+
+    
+    [Test]
+    public void Test_CalculateDiscount_DiscountOver100_ThrowsArgumentException()
+    {
+        // Arrange
+        decimal totalPrice = 100.0m;
+        decimal discount = 110.0m;
+
+        // Act & Assert
+        Assert.That(() => this._exceptions.ArgumentCalculateDiscount(totalPrice, discount), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_GetElement_ValidIndex_ReturnsElement()
+    {
+        // Arrange
+        int[] array = {1, 2, 3, 4 };
+        int index = 2;
+        int expectedIndex = 3;
+
+        //Act
+        int result = this._exceptions.IndexOutOfRangeGetElement(array, index);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expectedIndex));
+
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_GetElement_IndexLessThanZero_ThrowsIndexOutOfRangeException()
+    {
+        // Arrange
+        int[] array = { 1, 2, 3, 4 };
+        int index = - 2;
+        
+
+        // Act & Assert
+        Assert.That(() => this._exceptions.IndexOutOfRangeGetElement(array, index), Throws.InstanceOf<IndexOutOfRangeException>());
+    }
+
+    
+    [Test]
+    public void Test_GetElement_IndexEqualToArrayLength_ThrowsIndexOutOfRangeException()
+    {
+        // Arrange
+        int[] array = { 10, 20, 30, 40, 50 };
+        int index = array.Length;
+
+        // Act & Assert
+        Assert.That(() => this._exceptions.IndexOutOfRangeGetElement(array, index), Throws.InstanceOf<IndexOutOfRangeException>());
+    }
+
+    [Test]
+    public void Test_GetElement_IndexGreaterThanArrayLength_ThrowsIndexOutOfRangeException()
+    {
+        // Arrange
+        int[] array = { 10, 20, 30, 40, 50 };
+        int index = 10;
+
+        // Act & Assert
+        Assert.That(() => this._exceptions.IndexOutOfRangeGetElement(array, index), Throws.InstanceOf<IndexOutOfRangeException>());
+    }
+
+    [Test]
+    public void Test_PerformSecureOperation_UserLoggedIn_ReturnsUserLoggedInMessage()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_PerformSecureOperation_UserNotLoggedIn_ThrowsInvalidOperationException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_ParseInt_ValidInput_ReturnsParsedInteger()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_ParseInt_InvalidInput_ThrowsFormatException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_FindValueByKey_KeyExistsInDictionary_ReturnsValue()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_FindValueByKey_KeyDoesNotExistInDictionary_ThrowsKeyNotFoundException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_AddNumbers_NoOverflow_ReturnsSum()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_AddNumbers_PositiveOverflow_ThrowsOverflowException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_AddNumbers_NegativeOverflow_ThrowsOverflowException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_DivideNumbers_ValidDivision_ReturnsQuotient()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_DivideNumbers_DivideByZero_ThrowsDivideByZeroException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_SumCollectionElements_ValidCollectionAndIndex_ReturnsSum()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_SumCollectionElements_NullCollection_ThrowsArgumentNullException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_SumCollectionElements_IndexOutOfRange_ThrowsIndexOutOfRangeException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_GetElementAsNumber_ValidKey_ReturnsParsedNumber()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_GetElementAsNumber_KeyNotFound_ThrowsKeyNotFoundException()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_GetElementAsNumber_InvalidFormat_ThrowsFormatException()
+    {
+        // TODO: finish test
+    }
+}
