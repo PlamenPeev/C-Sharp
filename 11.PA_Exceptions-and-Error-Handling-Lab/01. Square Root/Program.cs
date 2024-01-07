af551d442d3eac523766b0ee9fbd26266524ee0d
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2024-01-07 21:00:22 +0200

    Add files via upload

---
+++ 11.PA_Exceptions-and-Error-Handling-Lab/01. Square Root/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            try
+            {
+                if (num < 0)
+                {
+                    throw new Exception();
+                }
+
+
+                double square = Math.Sqrt(num);
+                Console.WriteLine(square);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+            
+            
+        }
+    }
+}
---
+++ 11.PA_Exceptions-and-Error-Handling-Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02._Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            int currentNum = 1;
+            while(numbers.Count < 10)
+            {
+                try
+                {
+                    int num = ReadNumber(currentNum, 100);
+                    numbers.Add(num);
+                    currentNum = num;
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine($"Your number is not in range {currentNum} - 100!");
+                }
+                catch (FormatException)
+                {
+                    
+                    Console.WriteLine("Invalid Number!");
+                }
+               
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+        static int ReadNumber(int start, int end)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if(num <= start || num >= end)
+            {
+                throw new ArgumentException();
+            }
+
+            return num;
+        }
+    }
+}
